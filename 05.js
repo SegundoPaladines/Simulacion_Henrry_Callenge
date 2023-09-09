cfb607fe517133da26ea85e98dbe768aded1eea9
Segundo Paladines <segundopaladines@gmail.com>
2023-09-09 17:52:41 -0500

    05

--- 05.js
+++ 05.js
@@ -14,7 +14,11 @@ function pluck(array, propiedad) {
   // Pista: es una buena oportunidad para usar map.
 
   // Tu código acá:
-
+  var res = [];
+  array.forEach(element => {
+    res.push(element[propiedad]);
+  });
+  return res;
 }
 
 // No modifiques nada debajo de esta linea //
